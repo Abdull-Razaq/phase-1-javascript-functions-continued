0fcf232f8982015f1a2697f4d33fd065eea6fc72
Abdirizak Mohamed <abdirizakm123@gmail.com>
2022-11-24 19:15:09 +0300

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+
+function saturdayFun(fun = "roller-skate!") {
+    
+    if (fun === "roller-skate") {
+        
+        return("This Saturday, I want to ${fun}");
+
+    } else {
+        return("This Saturday, I want to bahte my dog");
+    }
+    
+}
+
+
+function mondayWork(work = "go to the office") {
+
+    return("This Monday, I will ${work}")
+    
+}
+
+function wrapAdjective(adjective="*"){
+    return function(word="special"){
+        return `You are ${adjective}${word}${adjective}!`
+    }
+ }
